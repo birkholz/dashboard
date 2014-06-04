2038762fcb7f3d9d8ad833fd5d84d175a5f42f13
Brandon Birkholz <brandonjbirkholz@gmail.com>
2014-06-03 23:56:07 -0700

    Initial commit of Flask/Angular version of dashboard

---
+++ dashboard.py
@@ -0,0 +1,54 @@
+from flask import Flask, Response, render_template, request, jsonify
+import requests
+
+api_key = ''
+api_secret = ''
+
+from localsettings import *
+
+app = Flask(__name__)
+
+headers = {
+	'X-Api-Key': api_key,
+	'X-Api-Secret': api_secret
+}
+
+
+@app.route('/')
+def index():
+	return Response(open('templates/index.html', 'r').read())
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+
+
+def api_request(url, data=None, params=None):
+	if (data):
+		r = requests.put(url, headers=headers, data=data)
+	else:
+		r = requests.get(url, headers=headers, params=params)
+	return Response(r.text, mimetype='application/json')
+
+
+@app.route('/get_user')
+def get_user():
+	url = 'https://api.assembla.com/v1/user.json'
+	return api_request(url)
+
+
+@app.route('/get_spaces')
+def get_spaces():
+	url = 'https://api.assembla.com/v1/spaces.json'
+	return api_request(url)
+
+
+@app.route('/get_tickets/<space_id>')
+def get_tickets(space_id):
+	url = 'https://api.assembla.com/v1/spaces/%s/tickets.json' % (space_id,)
+	return api_request(url, params={'report':8,'per_page':50})
+
+
+if __name__ == "__main__":
+    app.run(port=80)
